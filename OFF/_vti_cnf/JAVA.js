bc81030035688c9673a24b9f12847b9780d0fcf3
CHEMI <107860482+shMetrochemi@users.noreply.github.com>
2024-01-11 14:55:12 +0800

    Add files via upload

---
+++ OFF/_vti_cnf/JAVA.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|DESKTOP-1145141\\Administrator
+vti_modifiedby:SR|DESKTOP-1145141\\Administrator
+vti_timelastmodified:TR|09 Jan 2024 09:34:01 -0000
+vti_timecreated:TR|09 Jan 2024 09:33:35 -0000
+vti_extenderversion:SR|6.0.2.5516
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TW|09 Jan 2024 09:34:00 -0000
+vti_cacheddtm:TX|09 Jan 2024 09:34:00 -0000
+vti_filesize:IR|2508
